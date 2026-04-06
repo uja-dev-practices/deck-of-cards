5a232571a93264946049a24bb1aed8f8206bcab7
Mireya Cueto Garrido <mireya.cueto.0806@fernando3martos.com>
2026-04-06 10:42:42 +0200

    Añadida funcionalidad de intervalos en cartas blancas

--- backend/api/models/user_models.py
+++ backend/api/models/user_models.py
@@ -1,7 +1,11 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 
+
+# -----------------------------
+# MODELOS DE FUNCIONES DIFUSAS
+# -----------------------------
 
 class FuzzyTerm(BaseModel):
     term: str
@@ -11,17 +15,31 @@ class FuzzyTerm(BaseModel):
     right_nodes: List[List[float]]
 
 
+class IT2FuzzyTerm(BaseModel):
+    term: str
+    lower: FuzzyTerm
+    upper: FuzzyTerm
+
+
+# -----------------------------
+# HISTORIAL
+# -----------------------------
+
 class HistoryItem(BaseModel):
     id: Optional[str] = Field(default=None, alias="_id")
     name: str
     created_at: datetime
-    results: List[FuzzyTerm]
+    results: List[Union[FuzzyTerm, IT2FuzzyTerm]]
 
 
 class HistoryCreateRequest(BaseModel):
     name: str
-    results: List[FuzzyTerm]
+    results: List[Union[FuzzyTerm, IT2FuzzyTerm]]
 
+
+# -----------------------------
+# USUARIOS
+# -----------------------------
 
 class UserCreate(BaseModel):
     username: str
@@ -41,4 +59,3 @@ class UserInDB(BaseModel):
     password_hash: str
     token: Optional[str] = None
     history: List[HistoryItem] = []
-
